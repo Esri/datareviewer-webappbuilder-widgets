4aab22c805e5d05650d79d07b4de13b1c93865b9
Mona Sethi <msethi@esri.com>
2015-09-14 16:26:21 -0700

    Adding Dashboard Widget

---
+++ ReviewerDashboard/nls/strings.js
@@ -0,0 +1,22 @@
+define({
+  root:({
+    spatialFilterSelection: "Filter results by geography",
+    drawSelectionArea: "Draw a result selection area on the map",
+    currentSelection:"Show results only in current map extent",
+    censusBlockGroup:"Show results by geography",
+    noSpatialFilter:" Use no spatial filter",
+    reviewerSessionFilter:"Filter results by Reviewer Session",
+    selectAttributeFilters:"Select information to display",
+    phaseFieldName:"lifecyclephase",
+    statusFieldName:"lifecyclestatus",
+    allSessions:"All Sessions",
+    dashboardTab:"Dashboard",
+    filtersTab:"Settings",
+    noResults:"No Results Found",
+    resultsBy:"Results by: ",
+    reviewed: "Review",
+    corrected : "Correction",
+    verified: "Verification",
+    OtherCategory:"Other"
+  })
+});
---
+++ ReviewerDashboard/setting/nls/strings.js
@@ -0,0 +1,24 @@
+define({
+  root: ({
+    drsSOEURL: "Data Reviewer Server URL",
+    chartSection:"Number of Slices/Bars in the chart",
+    selectByGeography:"Include filter by geography",
+    selectUrl: "Service layer for filter by geography",
+    selectMapUrl: "Service for filter by geography",
+    geometryServiceURL: "Geometry service URL",
+    includeFieldNames:"Select Fields",
+    fieldNames:"Field Names",
+    lifecyclePhaseFieldName:"LIFECYCLEPHASE",
+    setSource:"Set",
+    defaultFieldName:"severity",
+    warning: "Incorrect input",
+    includeDefaultFieldName:"Default field name must be selected",
+    selectFieldWarning:"Alteast one field should be visible",
+    defaultFieldNotVisible: "Default field must be visible",
+    batchJobCheckGroup:"batchjobcheckgroup",
+    defaultColumn:"Default",
+    visibleColumn:"Visible",
+    fieldNameColumn:"Dashboard Field Name",
+    aliasColumn:"Alias"
+  })
+});
